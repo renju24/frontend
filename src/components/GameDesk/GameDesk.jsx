0fecd5124e5fa99f8bf68a26e1777bb0735e91b3
Aleksandra Smirnova <108838103+SashaSmirnovaSasha@users.noreply.github.com>
2022-11-11 00:13:36 +0300

    Create GameDesk.jsx

---
+++ src/components/GameDesk/GameDesk.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import classes from './GameDesk.module.css';
+
+import blackcircle from './pictures/blackcircle.png';
+import whitecircle from './pictures/whitecircle.png';
+import desk from './pictures/desk.png';
+
+const PlayerOne = (props) =>{
+
+}
+
+const PlayerTwo = (props) =>{
+    
+}
+
+
+const GameDesk = () => {
+    return (
+        <div className={classes.box}>
+            <div className={classes.players}>
+                <div className={classes.player1}><central>
+                    <img src={blackcircle} />
+                    <central><div className={classes.textT1}>
+                        <PlayerOne ></PlayerOne>
+                    </div></central>
+                    <a className={classes.textT1}>
+                        (Вы)
+                    </a>
+
+                </central></div >
+                <div className={classes.VS}>
+                    VS
+                </div>
+                <div className={classes.player2}>
+                    <img src={whitecircle} />
+                    <div className={classes.textT1}><central>
+                        Name2
+                    </central></div>
+                </div>
+                <div className={classes.title}>
+                    <central>
+                        Чей ход
+                    </central>
+                </div>
+
+            </div>
+            <div className={classes.desk}>
+                <img src={desk} />
+            </div>
+        </div>
+    )
+}
+
+export default GameDesk;
